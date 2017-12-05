1aa71fdac3f932c0ba772e44658d4fe36bc82f23
Anonymous Applicant <anonymous@example.com>
2017-12-05 15:45:41 -0700

    Use templating system for zip-name in submit

--- resources/leiningen/new/dw_code_exercise/submit.clj
+++ resources/leiningen/new/dw_code_exercise/submit.clj
@@ -3,20 +3,14 @@
             [clojure.string :as str])
   (:gen-class))
 
-(defn zip-name []
-  (-> (sh "pwd")
-      :out
-      (str/split #"/")
-      last
-      str/trim
-      (str ".zip")))
+(def zip-name "{{name}}.zip")
 
 (defn prepare-project []
   (sh "lein" "clean")
-  (println (:out (sh "zip" (zip-name) "-r" "."))))
+  (println (:out (sh "zip" zip-name "-r" "."))))
 
 (defn print-instructions []
-  (println (str "We just zipped up your work into the file '" (zip-name)
+  (println (str "We just zipped up your work into the file '" zip-name
                 "' in this directory. Please attach it to an email to"
                 " chris@democracy.works.")))
 
