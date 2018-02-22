58b86267d5734b527dae4a132cdba81e335bd69e
Christopher Shea <cmshea@gmail.com>
2018-02-22 16:10:04 -0500

    Version 3.0.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dw-code-exercise/lein-template "3.0.5"
+(defproject dw-code-exercise/lein-template "3.0.6-SNAPSHOT"
   :description "Democracy Works developer applicant coding exercise template"
   :url "https://github.com/democracyworks/dw-code-exercise-lein-template"
   :license {:name "Eclipse Public License"
