181391672a114fea5f90fbe2d10b994bdfa9989e
Christopher Shea <cmshea@gmail.com>
2018-02-22 16:02:24 -0500

    Update submit.clj

--- resources/leiningen/new/dw_code_exercise/submit.clj
+++ resources/leiningen/new/dw_code_exercise/submit.clj
@@ -12,7 +12,7 @@
 (defn print-instructions []
   (println (str "We just zipped up your work into the file '" zip-name
                 "' in this directory. Please attach it to an email to"
-                " chris@democracy.works.")))
+                " troy@democracy.works.")))
 
 (defn -main [& args]
   (prepare-project)
